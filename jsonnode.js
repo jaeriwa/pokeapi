027d426e278eacd3be9e58315e72ffbce5a66b72
Jamie Watterson <j.watterson@amaze.com>
2017-01-17 08:38:27 +0000

    Node API call not working but capturing where I am at with it

--- jsonnode.js
+++ jsonnode.js
@@ -1,32 +1,14 @@
 var fs = require("fs");
-var xhr = new XMLHttpRequest();
-var getJSON = function httpGet(theUrl, callback) {
-xhr.open('get', theUrl, true);
-xhr.responseType = 'json';
-xhr.onload = function (){
-	var status = xhr.status;
-	if (status == 200) {
-		callback(null, xhr.response);
-		} 	else {
-		calback(status);
-				}
-		}
-		xhr.send(null);
-	}
-	getJSON("http://pokeapi.co/api/v2/pokemon",
-function(err, data) {
-  if (err != null) {
-    console.log("Something went wrong: " + err);
-  } else {
-  	fs.writeFile('myjsonfile.json', data, 'utf8');
-  	console.log("saved ok");
-    // var parent = document.getElementById('voila');
-    // for(i=0; i < data.results.length; i++) {
-    // console.log(data.results[i].name);
-    // var p = document.createElement("p");
-    // p.innerHTML = data.results[i].name;
-    // p.className = "pokemon";
-    // parent.appendChild(p);
-    // }
-  }
-});
+var http = require('http');
+var options = {
+  	host: 'pokeapi.co',
+  	port : 80,
+  	path: '/api/v2/pokemon',
+  	method: 'GET'
+  };
+ var req = http.get(options, function(data){
+	// fs.writeFile('myjsonfile.json', response, 'utf8');
+	fs.writeFile('myjsonfile.json', data, 'utf8');
+	console.log(data);
+ });
+
