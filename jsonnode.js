c66cf70ce3dce3c2a182813507c246cc83be2d09
Jamie Watterson <j.watterson@amaze.com>
2017-01-17 15:47:49 +0000

    Sucessful using the request node instead

--- jsonnode.js
+++ jsonnode.js
@@ -1,14 +1,21 @@
 var fs = require("fs");
-var http = require('http');
-var options = {
-  	host: 'pokeapi.co',
-  	port : 80,
-  	path: '/api/v2/pokemon',
-  	method: 'GET'
-  };
- var req = http.get(options, function(data){
-	// fs.writeFile('myjsonfile.json', response, 'utf8');
-	fs.writeFile('myjsonfile.json', data, 'utf8');
-	console.log(data);
- });
+var request = require('request');
+request('http://pokeapi.co/api/v2/pokemon/1/', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log("Successful!");
+    fs.writeFile('myjsonfile.json', body, 'utf8'); 
+  }
+})
+// var http = require('http');
+// var options = {
+//   	host: 'pokeapi.co',
+//   	port : 80,
+//   	path: '/api/v2/pokemon',
+//   	method: 'GET'
+//   };
+//  var req = http.get(options, function(data){
+// 	// fs.writeFile('myjsonfile.json', response, 'utf8');
+// 	fs.writeFile('myjsonfile.json', data, 'utf8');
+// 	console.log(data);
+//  });
 
